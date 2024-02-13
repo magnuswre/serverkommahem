d13eed4ccb9dee75ddc1d0ecc9fe7e28157ead95
magnuswretblad <mwdev81@gmail.com>
2024-02-13 16:19:38 +0100

    work with auth on users

---
+++ authentication/auth.js
@@ -0,0 +1,8 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const secretKey = process.env.SECRET_KEY;
+
+exports.generateTokenUser = (user) => {
+    return jwt.sign({_id: user._id}, secretKey, {expiresIn: '1d'})
+}

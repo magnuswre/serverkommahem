fffae46bd5e3b7dbc3ca9ce6b9656a3c61430234
magnuswretblad <mwdev81@gmail.com>
2024-02-13 11:02:06 +0100

    made a lot ot changes. in template-product-current there is another way do change images in the media container for now its with the match variant id and image

--- migrations/20240213093202_v2.js
+++ migrations/20240213093202_v2.js
@@ -3,13 +3,38 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
+    return knex.schema.createTable('users', tbl => {
+      tbl.increments(); // primary key, called 'id' and make it auto-increment
+      // tbl.text('username', 120).notNullable().unique().index();
+      tbl.text('email', 120).notNullable().unique().index();  
+      tbl.text('password', 200).notNullable(); // 200 is the max length
+      // tbl.text('imageUrl', 128).notNullable(); // 128 is the max length
+      tbl.text('phone', 128).notNullable(); // 128 is the max length
+      tbl.timestamps(true, true); // adds created_at and updated_at columns
+    })
+    .createTable('destinations', tbl => {
+      tbl.increments();
+      // tbl.text('title').notNullable().index();
+      // tbl.text('description').notNullable();
+      tbl.text('enddestination').notNullable().index();
+      tbl.text('traveldate').notNullable();
+      tbl.text('seats').notNullable();
+      tbl.timestamps(true, true);     
+      tbl.integer('user_id')
+        .notNullable()
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE') // CASCADE, RESTRICT, DO NOTHING, SET NULL
+        .onUpdate('CASCADE');
+    })
+  };
   
-};
-
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
   
-};
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('destinations').dropTableIfExists('users');
+  };

3472aac2e42eee0d3da414438efe7bcc4000c9c2
magnuswretblad <mwdev81@gmail.com>
2024-02-13 10:35:14 +0100

    Made som changes in user table removed username added email and phone. Changed in destinationstable removed description and title added enddestination, traveldate and seats

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-   console.log(`Server running on port http: ${port}`);
+   console.log(`Server running on port http://localhost:${port}`);
 });

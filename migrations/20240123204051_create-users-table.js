b83c49f659f790d42f595d878c1cbf4fa78b9d86
magnuswretblad <mwdev81@gmail.com>
2024-02-15 12:43:52 +0100

    made a new knex migration

--- migrations/20240123204051_create-users-table.js
+++ migrations/20240123204051_create-users-table.js
@@ -4,17 +4,14 @@
  */
 exports.up = function(knex) {
   return knex.schema.createTable('users', tbl => {
-    tbl.increments(); // primary key, called 'id' and make it auto-increment
-    // tbl.text('username', 120).notNullable().unique().index();
+    tbl.increments(); // primary key, called 'id' and make it auto-incremen
     tbl.text('email', 120).notNullable().unique().index();  
     tbl.text('password', 200).notNullable(); // 200 is the max length
-    // tbl.text('imageUrl', 128).notNullable(); // 128 is the max length
     tbl.text('phone', 128).notNullable(); // 128 is the max length
     tbl.timestamps(true, true); // adds created_at and updated_at columns
   })
   .createTable('destinations', tbl => {
     tbl.increments();
-    // tbl.text('title').notNullable().index();
     tbl.text('enddestination').notNullable().index();
     tbl.text('traveldate').notNullable();
     tbl.text('arrivalTime').notNullable();

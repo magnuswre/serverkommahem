110320a7d3473d1732746611becb5a5acfcc2f71
magnuswretblad <mwdev81@gmail.com>
2024-06-07 16:26:35 +0200

    Refactor email template for password recovery

--- index.js
+++ index.js
@@ -42,12 +42,12 @@ function sendEmail({ recipient_email, OTP }) {
 <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
   <div style="margin:50px auto;width:70%;padding:20px 0">
     <div style="border-bottom:1px solid #eee">
-      <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Koding 101</a>
+      <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Kommahem.se</a>
     </div>
     
-    <p>Kommahem. Use the following OTP to complete your Password Recovery Procedure. OTP is valid for 5 minutes</p>
+    <p>Use the following OTP to complete your Password Recovery Procedure.</p>
     <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${OTP}</h2>
-    <p style="font-size:0.9em;">Regards,<br />Kommahem</p>
+    <p style="font-size:0.9em;">Kommahem.se</p>
     <hr style="border:none;border-top:1px solid #eee" />
     <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
       <p>Kommahem.se</p>

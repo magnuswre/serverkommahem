181152cb7cc1d4f692f08f233583a598f5c36b81
magnuswretblad <mwdev81@gmail.com>
2024-06-07 16:44:24 +0200

    chore: Refactor email template for password recovery

--- index.js
+++ index.js
@@ -30,33 +30,30 @@ function sendEmail({ recipient_email, OTP }) {
          to: recipient_email,
          subject: "Återställning lösenord Kommahem",
          html: `<!DOCTYPE html>
-<html lang="en" >
-<head>
-  <meta charset="UTF-8">
-  <title>Återställning lösenord Kommahem</title>
-  
-
-</head>
-<body>
-<!-- partial:index.partial.html -->
-<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
-  <div style="margin:50px auto;width:70%;padding:20px 0">
-    <div style="border-bottom:1px solid #eee">
-      <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Kommahem.se</a>
-    </div>
+         <html lang="en" >
+          <head>
+             <meta charset="UTF-8">
+             <title>Återställning lösenord Kommahem</title>
+           </head>
+          <body>
+          <!-- partial:index.partial.html -->
+            <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
+             <div style="margin:50px auto;width:70%;padding:20px 0">
+              <div style="border-bottom:1px solid #eee">
+               <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Kommahem.se</a>
+              </div>
     
-    <p>Use the following OTP to complete your Password Recovery Procedure.</p>
-    <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${OTP}</h2>
-    <p style="font-size:0.9em;">Kommahem.se</p>
-    <hr style="border:none;border-top:1px solid #eee" />
-    <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
-      <p>Kommahem.se</p>
-    </div>
-  </div>
-</div>
-<!-- partial -->
-  
-</body>
+             <p>Use the following OTP to complete your Password Recovery Procedure.</p>
+             <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${OTP}</h2>
+             <p style="font-size:0.9em;">Kommahem.se</p>
+             <hr style="border:none;border-top:1px solid #eee" />
+             <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
+             <p>Kommahem.se</p>
+            </div>
+          </div>
+       </div>
+      <!-- partial -->
+    </body>
 </html>`,
       }
       transporter.sendMail(mail_configs, function (error, info) {
